ba04c0d2d2254c8e8a06b7f8473178d09716c772
Ashikur Rahaman <ashikurrahaman9988@gmail.com>
2025-07-15 17:07:29 +0600

    responsive nav

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuList = document.getElementById("nav-links");
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "300px"
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+    }
+}
